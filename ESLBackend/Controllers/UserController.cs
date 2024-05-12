c01bc09608a9a1903541f427bd41b93cf3d8b71f
DurisDTU <nduris@hotmail.com>
2024-05-12 04:21:08 +0200

    Created controller for Crud methods for meeting room. Added models folder

--- ESLBackend/Controllers/UserController.cs
+++ ESLBackend/Controllers/UserController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using ESLBackend.Models;
 using ESLBackend.Utils;
 
 namespace ESLBackend.Controllers
