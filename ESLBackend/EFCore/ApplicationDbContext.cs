f08775fb4464f1769b0fed25cde38187eada0a8e
NikoDood <162916390+NikoDood@users.noreply.github.com>
2024-05-20 14:02:09 +0200

    user stuff

--- ESLBackend/EFCore/ApplicationDbContext.cs
+++ ESLBackend/EFCore/ApplicationDbContext.cs
@@ -11,6 +11,9 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     public DbSet<Templates.Upc> Upcs { get; set; }
     public DbSet<MeetingRoom> MeetingRooms { get; set; }
 
+    public new DbSet<User> Users { get; set; }
+    public DbSet<Organization> Organizations { get; set; }
+
 
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
@@ -37,5 +40,19 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
             .HasMany(t => t.Upcs)
             .WithOne(u => u.Templates)
             .HasForeignKey(u => u.TemplatesId);
+
+
+
+        modelBuilder.Entity<Organization>()
+     .HasKey(o => o.Id);
+
+
+
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Organization)
+            .WithMany() // Assuming an organization can have multiple users
+            .HasForeignKey(u => u.OrganizationId) // Use the foreign key property
+            .IsRequired(false); // Organization is optional
+
     }
 }
